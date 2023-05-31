6dfbc88f2d8543f8612bb5572bb16e2197f0f845
mango9236 <52327569+mango9236@users.noreply.github.com>
2023-05-31 13:20:41 +0900

    Update 둘만의 암호.py

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,21 +1,22 @@
 def solution(s, skip, index):
     skip = list(skip)
-    alphabet = [] # 97~122 (a~z)
+    alphabet = [] 
+    
+    # 97~122 (a~z)
     for i in range(97,123):
         alphabet.append(chr(i))
-        
+    
+    # skip단어 제거
     for skip_word in skip:
         alphabet.remove(skip_word)
     
-    print(alphabet)
-    print(len(alphabet))
     result = ""
     for letter in s:
         idx = alphabet.index(letter)
         new_idx = idx + index
         
         if new_idx > len(alphabet)-1: # 0~len-1까지 문자가있으므로
-            new_idx %= len(alphabet)
+            new_idx %= len(alphabet) # index가 큰 값일 수도 있으므로 -= 말고 %=
             
         result += alphabet[new_idx]
         
