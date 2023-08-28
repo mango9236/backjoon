26e0828bf7907a51d530f3bb07ccc5883a2bc55b
mingyu choi <52327569+mango9236@users.noreply.github.com>
2023-08-28 20:34:25 +0900

    [Silver II] Title: 에디터, Time: 308 ms, Memory: 42140 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,33 +1,29 @@
-import sys 
+import sys
 input = sys.stdin.readline
-
-left = list(input().rstrip())
-length = len(left)
+left = list(input().strip('\n'))
 right = []
-
 n = int(input())
+
 for i in range(n):
-    command = input().rstrip().split()
-    if command[0] == "L":
-        if len(left) == 0:
-            continue
-        right.append(left.pop())
+    command=input().split()
+
+    if command[0]=="L":
+        if len(left) !=0:
+            right.append(left.pop())
 
     if command[0] == "D":
-        if len(right) == 0:
-            continue
-        left.append(right.pop())
+        if len(right)!=0:
+            left.append(right.pop())
 
-    if command[0] == "B":
-        if len(left) == 0:
-            continue
-        left.pop()
+    if command[0]=="B":
+        if len(left) != 0:
+            left.pop()
 
-    if command[0] == "P":
+    if command[0]=="P":
         left.append(command[1])
 
-for i in left:
-    print(i, end="")
 
-for i in range(len(right)):
-    print(right.pop(), end="")    
+left.extend(reversed(right))
+
+
+print(''.join(map(str,left)))
