94a7fc773bc2e620e3f6228ce8c38d71b138e474
mingyu choi <52327569+mango9236@users.noreply.github.com>
2023-10-06 14:12:43 +0900

    답안 2 추가

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,30 +1,29 @@
 def solution(participant, completion):
-#     dic = {}
-#     answer = ""
-#     # 참가 시 1 증가
-#     for i in participant:
-#         # 이미 있을 경우 cnt++
-#         if i in dic:
-#             dic[i] += 1
-#         else:    
-#             dic[i] = 1
+    dic = {}
+    answer = ""
+    # 참가 시 1 증가
+    for i in participant:
+        # 이미 있을 경우 cnt++
+        if i in dic:
+            dic[i] += 1
+        else:    
+            dic[i] = 1
 
-#     # 완주 시 다시 감소
-#     for i in completion:
-#         dic[i] -= 1
+    # 완주 시 다시 감소
+    for i in completion:
+        dic[i] -= 1
         
-#     for i in dic:
-#         if dic[i] == 1:
-#             return i
+    for i in dic:
+        if dic[i] == 1:
+            return i
 
         
-    '''답안 2'''
-    '''정렬을 활용'''
+'''답안 2'''
+'''정렬을 활용, 효율성은 좀 떨어짐'''
+#     participant = sorted(participant)
+#     completion = sorted(completion)
+#     completion.append("")
     
-    participant = sorted(participant)
-    completion = sorted(completion)
-    completion.append("")
-    
-    for i,v in enumerate(participant):
-        if v != completion[i]:
-            return v
+#     for i,v in enumerate(participant):
+#         if v != completion[i]:
+#             return v
